e2ecbce3973a8e87065a59df4f686cde5613badf
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-04-14 14:25:09 +0100

    added filtering of categories on home page

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,21 +1,42 @@
 import { useEffect, useState } from "react";
 import { Item } from "./Item";
 
-export const ItemList = () => {
+export const ItemList = ({ category }) => {
   const [showItems, setShowItems] = useState([]);
+  const [selectCategory, setSelectCategory] = useState("");
 
   useEffect(() => {
-    fetch("https://marketplace-api-72935.herokuapp.com/api/items")
+    let fetchString = `https://marketplace-api-72935.herokuapp.com/api/items`
+
+    if (selectCategory !== "select") {
+      fetchString += `?category_name=${selectCategory}`
+    }
+
+    fetch(fetchString)
       .then((res) => {
         return res.json();
       })
       .then((data) => {
         setShowItems(data.items);
       });
-  }, []);
+  }, [selectCategory]);
+
+  console.log(category, "<< category")
 
   return (
     <div>
+      <select name="selector" id="" onChange={(event) => {
+        setSelectCategory(event.target.value)
+      }}>
+        <option value="select" key="select">Select Category</option>
+        {category.map((cat) => {
+          return (
+            <option value={cat.category_name} key={cat.category_name}>
+              {cat.category_name}
+            </option>
+          );
+        })}
+      </select>
       <Item items={showItems} />
     </div>
   );
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { ItemList } from "../components/ItemList";
+import { useState, useEffect } from "react";
 
 export const Home = () => {
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+    fetch("https://marketplace-api-72935.herokuapp.com/api/categories")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data.categories)
+        setCategory(data.categories);
+      });
+  }, []);
+
   return (
     <div>
       Home
-      <ItemList />
+     
+      <ItemList category={category}/>
     </div>
   );
 };
